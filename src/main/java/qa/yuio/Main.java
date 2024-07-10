aec10d97cbdc443cdc9b653ebe887b56869350a3
yuioqa <yuio00qa@gmail.com>
2024-07-10 13:36:59 +0300

    Update Main.java

--- src/main/java/qa/yuio/Main.java
+++ src/main/java/qa/yuio/Main.java
@@ -2,6 +2,6 @@ package qa.yuio;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет qa.guru!");
+        System.out.println("Привет qa.guru!!!");
     }
 }
